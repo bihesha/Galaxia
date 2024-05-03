99bdae39080d8ad085f2f7de60f43dbc3fd3c9ca
Bihesha Dilshan <132125114+bihesha@users.noreply.github.com>
2024-05-03 23:58:01 +0530

    Delete frontend directory

--- frontend/src/components/__test__/apod.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import APOD from '../APOD/apod';
-
-test('renders A Picture of the Day title', () => {
-    render(<APOD/>);
-    const headingElement = screen.getByText('A Picture of the Day');
-    expect(headingElement).toBeInTheDocument();
-});
